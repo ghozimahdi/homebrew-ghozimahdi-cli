d911c35f0cf8d9de946cdc8fd7cdbf5e71dc828b
mbahgojol <profghozimahdi@gmail.com>
2024-11-24 15:57:41 +0700

    Update gm formula for version 1.1.3

--- gm.rb
+++ gm.rb
@@ -1,9 +1,9 @@
 class Gm < Formula
     desc "CLI sederhana untuk mengelola modul Melos"
     homepage "https://github.com/mbahgojol/homebrew-ghozimahdi-cli"
-    url "https://github.com/mbahgojol/homebrew-ghozimahdi-cli/releases/download/v1.1.2/gm-v112.tar.gz"
-    sha256 "0a0d8205a08d5534b2adb85c331492d0e858471509f7fe0a0f31693ded20eec4"
-    version "1.1.2"
+    url "https://github.com/mbahgojol/homebrew-ghozimahdi-cli/releases/download/v1.1.3/gm-v113.tar.gz"
+    sha256 "0bc9363e96f4fe53fc3143760a6d18063e2eb21a0bdc68241c0c4f67b1723665"
+    version "1.1.3"
   
     def install
       bin.install "gm"
