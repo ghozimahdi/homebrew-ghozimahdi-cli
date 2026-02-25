c506baedef954359df48c3ac80ef6d05056cc280
ghozimahdi <ghozi.dev@gmail.com>
2026-02-26 06:22:56 +0700

    feat: update version to 4.0.0 and corresponding URLs

--- gm.rb
+++ gm.rb
@@ -9,6 +9,7 @@ class Gm < Formula
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/ghozimahdi/homebrew-ghozimahdi-cli/releases/download/v4.0.0/gm-aarch64-apple-darwin.tar.gz"
     sha256 "aae9f32d3506f4c6e9ced60ffd0b7d1f40b435652553be6ae5ff1363d3b8607e"
+  end
 
   def install
     bin.install "gm"
